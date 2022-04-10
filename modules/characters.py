057ba9f6103865525cc330a33f400074cdee8942
StephenB92 <stephennb92@gmail.com>
2022-04-10 12:39:45 +0000

    Define character statistics in separate module

--- modules/characters.py
+++ modules/characters.py
@@ -1,5 +1,48 @@
 # Player Statistics
 
+hero = {
+    'health': 100,
+    'attack': [10, 18],
+    'potion': 0,
+    'revive': 0
+}
+
 # Enemy Statistics
 
+# Tutorial enemy
+tutorial_enemy = {
+    'health': 10,
+    'attack': 0
+}
+
+# Enemy 1
+undead_soldier = {
+    'health': 30,
+    'attack': [8, 12]
+}
+
+# Enemy 2
+undead_knight = {
+    'health': 50,
+    'attack': [12, 20]
+}
+
 # Boss statistics
+
+# First Boss
+boss_1 = {
+    'health': 60,
+    'attack': [15, 25]
+}
+
+# Second Boss
+boss_2 = {
+    'health': 80,
+    'attack': [20, 30]
+}
+
+# Final Boss
+final_boss = {
+    'health': 120,
+    'attack': [25, 35]
+}
